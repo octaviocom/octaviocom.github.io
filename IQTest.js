a80462c680766ef3324eab9e090af7ee03ac7186
octaviocom <138821929+octaviocom@users.noreply.github.com>
2023-07-10 13:09:28 -0500

    Add files via upload

--- IQTest.js
+++ IQTest.js
@@ -31,7 +31,7 @@ createCanvas(windowWidth, windowHeight)
 textAlign(CENTER)
 imageMode(CENTER)
 
-QuestionIBool = false
+
 
 
 
@@ -39,35 +39,21 @@ QuestionIBool = false
 
 function draw(){
 	print(mouseX, mouseY)
-	if (startScreenBool = true){
-		startScreen()
 
-	}
-
-	if (QuestionIBool = true){
-		QuestionI()
-	}else{
+	if(startScreenBool = true){
 		startScreen()
 	}
-
-	if (QuestionIIBool = true){
-		
-	}else{
-		startScreen()
-	}
-
-	if(QuestionIIIBool = true){
-		
-
-	}else{
-		startScreen()
-	}
+	
 	
 
 }
 
+
+function nextScene(){
+
+}
+
 function startScreen(){
-	//add images with brains and intelligence ;)
 		image(elegant, windowWidth/2, windowHeight/2, 1640, 1000 )
 		textSize(40)
 		fill(80, 91, 173)
@@ -78,9 +64,15 @@ function startScreen(){
 		text('Designed by: Octavio Combellas-Jaimes', windowWidth/2, windowHeight/4+50)
 		image(brain, windowWidth/4 - 30, windowHeight/2, 350, 300)
 		image(intelligence , windowWidth*3/4+30, windowHeight/2, 350, 300)
+		if(keyPressed){
+			startScreenBool = false
+			QuestionIBool = false
+
+		}
+
 		
 }
 
-function QuestionI(){
-	image(question, windowWidth/2, windowHeight/2, 1640, 1000)
-}
+// function QuestionI(){
+// 	image(question, windowWidth/2, windowHeight/2, 1640, 1000)
+// }
